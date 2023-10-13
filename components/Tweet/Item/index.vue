9f3651daa9cde17f8ac67fb26dffd2ae070f5161
Xaninho <alexandre1999neves@gmail.com>
2023-10-13 09:25:36 +0100

    🥪 WIP: Home Classes

---
+++ components/Tweet/Item/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+
+        <div class="ml-16">
+
+            <p class="flex-shrink font-medium text-gray-800 w-auto dark:text-white">
+                {{ $props.tweet.text }}
+            </p>
+
+            <div v-for="image in tweet.mediaFiles"
+                :key="image.id"
+                class="flex my-3 mr-2 border-2 rounded-2xl"
+                :class="mealshotsBorderColor">
+                <img :src="image.url" />
+            </div>
+
+
+        </div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+const {mealshotsBorderColor} = useTailwindConfig();
+
+const props = defineProps({
+    tweet: {
+        type: Object,
+        required: true
+    }
+})
+
+</script>
